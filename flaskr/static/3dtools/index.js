0a65b559f10263bc92dea390d164ef8fd100e0cf
bennieHan <bennieHan98@gmail.com>
2020-03-10 10:55:24 +0800

    zhanghan 20/03/10

--- flaskr/static/3dtools/index.js
+++ flaskr/static/3dtools/index.js
@@ -1,7 +1,14 @@
 /* global sharkViewer */
 let s = null;
-let gold_swc = null
-let test_swc = null
+let gold_swc = null;
+let test_swc = null;
+
+let gold_txt = null;
+let test_txt = null;
+
+let method = 2;
+let rad_threshold = 'default'
+let len_threshold = 'default'
 
 let mdata;
 function readGoldSwcFile(e) {
@@ -9,8 +16,8 @@ function readGoldSwcFile(e) {
   if (f) {
     const r = new FileReader();
     r.onload = (e2) => {
-      const swcTxt = e2.target.result;
-      gold_swc = sharkViewer.swcParser(swcTxt);
+      gold_txt = e2.target.result;
+      gold_swc = sharkViewer.swcParser(gold_txt);
       if (Object.keys(gold_swc).length > 0) {
         s.unloadNeuron('gold');
         s.loadNeuron('gold', null, gold_swc);
@@ -30,9 +37,9 @@ function readTestSwcFile(e) {
   if (f) {
     const r = new FileReader();
     r.onload = (e2) => {
-      const swcTxt = e2.target.result;
-      test_swc = sharkViewer.swcParser(swcTxt);
-      if (Object.keys(gold_swc).length > 0) {
+      test_txt = e2.target.result;
+      test_swc = sharkViewer.swcParser(test_txt);
+      if (Object.keys(test_swc).length > 0) {
         s.unloadNeuron('test');
         s.loadNeuron('test', null, test_swc);
         s.render();
@@ -58,18 +65,6 @@ function readObjFile(e) {
     reader.readAsText(file);
   }
 }
-
-function click_swc_input() {
-    document.getElementById("swc_input")
-            .click();
-}
-
-$(document).ready(function(){
-    $('#mask_1').click(function(){
-        $('#swc_input').click();
-    });
-
-});
 
 window.onload = () => {
   document
@@ -98,3 +93,47 @@ window.onload = () => {
   s.loadNeuron('swc', null, swc);
   s.render();
 };
+
+function do_post_ch_method(to, p){
+    var myForm = document.createElement("form");
+    myForm.method = "post";
+    myForm.action = to;
+
+    var gold_swc_input = document.createElement("input");
+    var text_swc_input = document.createElement("input");
+    var method_input = document.createElement("input");
+    var rad_threshold_input = document.createElement("input");
+    var len_threshold_input = document.createElement("input");
+    if (gold_txt != null) {
+        gold_txt = gold_txt.replace(/[;]/g," ");
+        gold_txt = gold_txt.replace(/[\n\r]/g,";");
+    }
+    if (test_txt != null) {
+        test_txt = test_txt.replace(/[;]/g," ");
+        test_txt = test_txt.replace(/[\n\r]/g,";")
+    }
+
+    gold_swc_input.setAttribute("name", "gold_swc");
+    gold_swc_input.setAttribute("value", gold_txt);
+    text_swc_input.setAttribute("name", "test_swc");
+    text_swc_input.setAttribute("value", test_txt);
+
+    if (p == "geometry"){
+        method_input.setAttribute("name", "method");
+        method_input.setAttribute("value", method);
+        rad_threshold_input.setAttribute("name", "rad_threshold");
+        rad_threshold_input.setAttribute("value", rad_threshold);
+        len_threshold_input.setAttribute("name", "len_threshold");
+        len_threshold_input.setAttribute("value", len_threshold);
+    }
+
+    myForm.appendChild(gold_swc_input);
+    myForm.appendChild(text_swc_input);
+    myForm.appendChild(method_input);
+    myForm.appendChild(rad_threshold_input);
+    myForm.appendChild(len_threshold_input);
+
+    document.body.appendChild(myForm);
+    myForm.submit();
+    document.body.removeChild(myForm);
+}
--- flaskr/web_test.py
+++ flaskr/web_test.py
@@ -1,5 +1,11 @@
-from flask import Flask, escape, url_for, request, render_template
+import sys
+
+from flask import Flask, request, render_template
+from pymets.metric.length_metric import web_length_metric
+
 app = Flask(__name__)
+
+ALLOWED_EXTENSIONS = {'swc', 'txt'}
 
 
 @app.route('/')
@@ -7,19 +13,27 @@ def hello(name=None):
     return render_template('webmets.html', name=name)
 
 
-def valid_login(username, password):
-    return True
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            print("YES")
-        else:
-            error = 'Invalid username/password'
-    # the code below is executed if the request method
-    # was GET or the credentials were invalid
-    return render_template('flexStudy', error=error)
+@app.route('/geometry', methods=['POST'])
+def geometry():
+    gold_swc = request.form['gold_swc']
+    test_swc = request.form['test_swc']
+    method = request.form['method']
+    rad_threshold = request.form['rad_threshold']
+    len_threshold = request.form['len_threshold']
+
+    recall, precision, swc_gold_str = web_length_metric(gold_swc=gold_swc,
+                                                        test_swc=test_swc,
+                                                        method=int(method),
+                                                        rad_threshold=rad_threshold,
+                                                        len_threshold=len_threshold)
+    print(recall, precision)
+    return render_template('webmets.html', recall=recall, precision=precision, )
+
+
+if __name__ == "__main__":
+    pass
